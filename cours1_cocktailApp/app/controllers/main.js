4445f60f38eb86081c69ce3cafb34d60ed1fbe48
J.Nexus <46869823+nexus9111@users.noreply.github.com>
2022-12-06 11:44:58 +0100

    :tada: push cours 1

---
+++ cours1_cocktailApp/app/controllers/main.js
@@ -0,0 +1,57 @@
+const root = document.getElementById('root');
+
+const button = document.createElement('button');
+button.classList.add('btn');
+button.classList.add('btn-primary');
+button.textContent = "Click me!";
+
+createCocktailCardContent = (image, name, subtitle, description) => {
+    return `
+    <div class="card" style="width: 18rem;">
+        <img src="${image}" class="card-img-top" alt="${name}">
+        <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${subtitle}</h6>
+            <p>${description}</p>
+        </div>
+    </div>`;
+}
+
+getRadomCocktailInfo = async () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+            const data = await response.json();
+            randomCocktail = data.drinks[0];
+            return resolve(randomCocktail);
+        } catch (error) {
+            return reject("Error while fetching data");
+        }
+    });
+}
+
+createRandomCocktailCard = async () => {
+    try {
+        randomCocktail = await getRadomCocktailInfo();
+        cardContent = createCocktailCardContent(
+            randomCocktail.strDrinkThumb, 
+            randomCocktail.strDrink, 
+            randomCocktail.strCategory,
+            randomCocktail.strInstructions);
+        if (document.querySelector('.cocktailDiv')) {
+            card = document.querySelector('.cocktailDiv');
+            card.innerHTML = cardContent;
+            return;
+        }
+        div = document.createElement('div');
+        div.classList.add('cocktailDiv');
+        div.innerHTML = cardContent;
+        root.appendChild(div);
+    } catch (error) {
+        alert(error);
+    }
+}
+
+button.addEventListener('click', createRandomCocktailCard);
+
+root.appendChild(button);
