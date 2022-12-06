b047cfa4d222fb9aeaa643248a5e604a7cf7373d
J.Nexus <46869823+nexus9111@users.noreply.github.com>
2022-12-06 12:08:17 +0100

    :tada: increase code

--- cours1_cocktailApp/app/controllers/main.js
+++ cours1_cocktailApp/app/controllers/main.js
@@ -5,7 +5,24 @@ button.classList.add('btn');
 button.classList.add('btn-primary');
 button.textContent = "Click me!";
 
-const createCocktailCardContent = (image, name, subtitle, description) => {
+const getIngredients = (drink) => {
+    ingredients = [];
+    for (const key in drink) {
+        if (key.includes('strIngredient') && drink[key]) {
+            ingredients.push(drink[key]);
+        }
+    }
+    return ingredients;
+}
+
+const createCocktailCardContent = (image, name, subtitle, description, ingredientsList) => {
+
+    ingredients = `<ol class="list-group list-group-numbered">`
+    for (const ingredient of ingredientsList) {
+        ingredients += `<li class="list-group-item">${ingredient}</li>`
+    }
+    ingredients += `</ol>`
+
     return `
     <div class="card" style="width: 18rem;">
         <img src="${image}" class="card-img-top" alt="${name}">
@@ -13,6 +30,7 @@ const createCocktailCardContent = (image, name, subtitle, description) => {
             <h5 class="card-title">${name}</h5>
             <h6 class="card-subtitle mb-2 text-muted">${subtitle}</h6>
             <p>${description}</p>
+            ${ingredients}
         </div>
     </div>`;
 }
@@ -22,6 +40,7 @@ const getRadomCocktailInfo = async () => {
         const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
         const data = await response.json();
         randomCocktail = data.drinks[0];
+        console.log(randomCocktail);
         return randomCocktail;
     } catch (error) {
         throw new Error("An error occured while fetching the cocktail data");
@@ -35,7 +54,8 @@ const createRandomCocktailCard = async () => {
             randomCocktail.strDrinkThumb,
             randomCocktail.strDrink,
             randomCocktail.strCategory,
-            randomCocktail.strInstructions);
+            randomCocktail.strInstructions,
+            getIngredients(randomCocktail));
         if (document.querySelector('.cocktailDiv')) {
             card = document.querySelector('.cocktailDiv');
             card.innerHTML = cardContent;
